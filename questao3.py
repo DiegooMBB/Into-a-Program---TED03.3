1910f262807b44bc22fbfb124f3867df43410698
DiegooMBB <128840164+DiegooMBB@users.noreply.github.com>
2023-03-25 14:36:47 -0300

    Add files via upload

---
+++ questao3.py
@@ -0,0 +1,4 @@
+print('=========CONVITE DA FESTA DO P1-A=========')                                  #QUESTÃO 4
+nome=input('Digite seu nome: ')                                                      #QUESTÃO 4
+nome = nome.upper()                                                                  #QUESTÃO 4
+print('Olá,', nome,'. Você foi convidado para a festa da Turma do P1-A.')            #QUESTÃO 4
