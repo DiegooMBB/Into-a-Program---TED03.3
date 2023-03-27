cc86ef55acd164d9d71dc30082d42854355b9d61
DiegooMBB <128840164+DiegooMBB@users.noreply.github.com>
2023-03-27 20:56:00 -0300

    Update questao2.py

--- questao2.py
+++ questao2.py
@@ -1,5 +1,2 @@
-n1=float(input('Digite um numero: '))        #QUESTÃO 3
-n2=1357                                      #QUESTÃO 3
-PE1= ((n1+1357)*8)/5                         #QUESTÃO 3
-PE1=(PE1)**2                                 #QUESTÃO 3
-print('O resultado da equação é:', PE1,'.')  #QUESTÃO 3
+nome=str(input('Digite seu nome: '))
+print('Bom dia, %s.'%nome)
