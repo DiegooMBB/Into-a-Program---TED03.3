bd0a872c417b2aa6455c51dc3a1f385b083c9417
DiegooMBB <128840164+DiegooMBB@users.noreply.github.com>
2023-03-27 20:57:22 -0300

    Update questao4.py

--- questao4.py
+++ questao4.py
@@ -1,9 +1,5 @@
-v1=2                                                                    #QUESTÃO 5
-soma= v1+6                                                              #QUESTÃO 5
-sub= 10-v1                                                              #QUESTÃO 5
-multi= v1*4                                                             #QUESTÃO 5
-div= 16/v1                                                              #QUESTÃO 5
-print('O resultado da soma dos valores é:', soma,'.')                   #QUESTÃO 5
-print('O resultado da subtração dos valores é:', sub,'.')               #QUESTÃO 5
-print('O resultado da multiplicação dos valores é:', multi,'.')         #QUESTÃO 5
-print('O resultado da divisão dos valores é: {:.0f}'.format(div),'.')   #QUESTÃO 5
+print('==========CONVITE DE FESTA==========')
+nome=str(input('Digite seu nome: '))
+nome=nome.upper()
+print('Olá, %s. Você foi convidado para a festa.'%nome)
+print('====================================')
