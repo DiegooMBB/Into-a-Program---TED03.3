ad5f5c1ec56780c1bbef5e8349f5792454775465
DiegooMBB <128840164+DiegooMBB@users.noreply.github.com>
2023-03-27 20:48:36 -0300

    Update questao1.py

--- questao1.py
+++ questao1.py
@@ -1,8 +1,1 @@
-print('=================Boletim do aluno=================')                             #QUESTÃO 2
-nota1=float(input('Digite sua primeira nota: '))                                        #QUESTÃO 2
-nota2=float(input('Digite sua segunda nota: '))                                         #QUESTÃO 2
-medianotas = (nota1+nota2)/2                                                            #QUESTÃO 2
-if medianotas >= 6:                                                                     #QUESTÃO 2
-    print('A sua média foi de {:.2f}'.format(medianotas),' e você está aprovado.')      #QUESTÃO 2
-else:                                                                                   #QUESTÃO 2
-    print('A sua média foi de {:.2f}'.format(medianotas),' e você está reprovado.')     #QUESTÃO 2
+print('EU AMO PROGRAMAR EM PYTHON.')
